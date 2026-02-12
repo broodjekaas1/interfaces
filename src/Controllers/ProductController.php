68412a60133e58ccb56a791feead961c6f041bee
broodjekaas1 <chickenpow4@gmail.com>
2026-02-12 12:14:02 +0100

    files

---
+++ src/Controllers/ProductController.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use GuzzleHttp\Psr7\Utils;
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class ProductController
+{
+    public function index(): ResponseInterface
+    {
+        $stream = Utils::streamFor("List of products");
+
+        $response = new Response;
+
+        $response = $response->withBody($stream);
+
+        return $response;
+    }
+
+    public function show(ServerRequestInterface $request, array $args): ResponseInterface
+    {
+        $id = $args["id"];
+
+        $stream = Utils::streamFor("Single product with ID $id");
+
+        $response = new Response;
+
+        $response = $response->withBody($stream);
+
+        return $response;
+    }
+}
